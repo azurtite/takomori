8eccb5a8f45839b309986a27abce6e33b46f1e15
mozukuSu <mail.dev.azurite@gmail.com>
2022-11-14 22:50:39 +0900

    不要なコードを削除

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  handleCounter: (callback) => ipcRenderer.on('update-counter', callback),
   minimizeWindow: (callback) => ipcRenderer.send('minimize-window', callback)
 })

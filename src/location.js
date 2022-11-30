ddcf345fe654885391892626b1d5ecb043b2671d
mozukuSu <mail.dev.azurite@gmail.com>
2022-11-30 21:29:32 +0900

    設定ファイルを追加

---
+++ src/location.js
@@ -0,0 +1,7 @@
+// Change baseURL and apiKey according to your environment
+// ex)
+// http://localhost/api/files?apikey=241B873D3FF8408FB95E1DB8510F81C
+// ~~~~~~~~~~~~~~~~~                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// <--  baseURL  -->                 <--          apiKey         -->
+let baseURL	= 'http://octopi.local/';
+let apiKey	= 'api-key';

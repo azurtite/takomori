48e31e9f5d0d62b53ade9959cfb17038cc02af27
mozukuSu <mail.dev.azurite@gmail.com>
2022-12-22 16:32:28 +0900

    Code update.

--- src/location.js
+++ src/location.js
@@ -3,7 +3,5 @@
 // http://localhost/api/files?apikey=241B873D3FF8408FB95E1DB8510F81C
 // ~~~~~~~~~~~~~~~~~                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // <--  baseURL  -->                 <--          apiKey         -->
-//let baseURL	= 'http://192.168.0.14/';
-//let apiKey	= 'E78CBB6A91D043868745FBA61AF0315F';	//192.168.0.14
-let baseURL	= 'http://192.168.0.18/';
-let apiKey	= '9AB708BC5B664572880CEC96214FD4C1';	//192.168.0.18
+let baseURL	= 'http://192.168.0.14/';
+let apiKey	= 'E78CBB6A91D043868745FBA61AF0315F';

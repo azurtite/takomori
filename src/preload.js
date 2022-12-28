33665866807df44e31bff8293237bc943d8c577e
mozukuSu <mail.dev.azurite@gmail.com>
2022-12-28 22:20:52 +0900

    Add interprocess communication.

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  minimizeWindow: (callback) => ipcRenderer.send('minimize-window', callback)
+  minimizeWindow: (callback) => ipcRenderer.send('minimize-window', callback),
+  changeWindowPosition: (callback) => ipcRenderer.send('win-position-set', callback)
 })
